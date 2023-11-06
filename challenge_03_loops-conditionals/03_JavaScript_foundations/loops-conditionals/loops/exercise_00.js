41fc2d2e69add9743b0a1941e9676d81b0f6c6d5
Adriana Garcia-Rodriguez <adriana.garcia@mataro.epiaedu.cat>
2023-11-06 19:34:15 +0100

    Refactor code for better readability and user experience

--- challenge_03_loops-conditionals/03_JavaScript_foundations/loops-conditionals/loops/exercise_00.js
+++ challenge_03_loops-conditionals/03_JavaScript_foundations/loops-conditionals/loops/exercise_00.js
@@ -4,29 +4,29 @@
     2. Create a for loop that will print out all the odd numbers between 10 and 40.
 */
 
-print("Select an option: ");
-print("1. Even numbers between 10 and 40");
-print("2. Odd numbers between 10 and 40");
+console.log("Select an option: ");
+console.log("1. Even numbers between 10 and 40");
+console.log("2. Odd numbers between 10 and 40");
 
-switch (prompt("Enter your choice: ")) {
-  case "1":
-    evenNumbers();
-    break;
-  case "2":
-    oddNumbers();
-    break;
+switch (prompt("Enter your option: ") {
+    case "1":
+        evenNumbers();
+        break;
+    case "2":
+        oddNumbers();
+        break;
 }
 
 //even numbers
-for (let i = 10; i <= 40; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
+for (let number = 10; number <= 40; number++) {
+    if (number % 2 === 0) {
+        console.log(number);
+    }
 }
 
 //odd numbers
-for (let i = 10; i <= 40; i++) {
-  if (i % 2 !== 0) {
-    console.log(i);
-  }
+for (let number = 10; number <= 40; number++) {
+    if (number % 2 !== 0) {
+        console.log(number);
+    }
 }
--- challenge_03_loops-conditionals/03_JavaScript_foundations/loops-conditionals/while/exercise_01.js
+++ challenge_03_loops-conditionals/03_JavaScript_foundations/loops-conditionals/while/exercise_01.js
@@ -4,33 +4,37 @@
     2. Create a while loop that will print out all the odd numbers between 10 and 40.
 */
 
-print("Select an option: ");
-print("1. Even numbers between 10 and 40");
-print("2. Odd numbers between 10 and 40");
+console.log("Select an option: ");
+console.log("1. Even numbers between 10 and 40");
+console.log("2. Odd numbers between 10 and 40");
 
-switch (prompt("Enter your choice: ")) {
-  case "1":
-    evenNumbers();
-    break;
-  case "2":
-    oddNumbers();
-    break;
+switch ("Enter your choice: ") {
+    case "1":
+        evenNumbers();
+        break;
+    case "2":
+        oddNumbers();
+        break;
 }
 
-//even numbers
-let i = 10;
-while (i <= 40) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-  i++;
+function evenNumbers() {
+    //even numbers
+    let i = 10;
+    while (i <= 40) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+        i++;
+    }
 }
 
-//odd numbers
-i = 10;
-while (i <= 40) {
-  if (i % 2 !== 0) {
-    console.log(i);
-  }
-  i++;
+function oddNumbers() {
+    //odd numbers
+    i = 10;
+    while (i <= 40) {
+        if (i % 2 !== 0) {
+            console.log(i);
+        }
+        i++;
+    }
 }
